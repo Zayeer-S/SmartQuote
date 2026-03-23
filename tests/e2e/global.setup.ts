336a811c07c1d1248dc82cfccdc4732cb309b102
Zayeer-S <zayeersultan@gmail.com>
2026-03-23 21:52:22 +0000

    fix: have secrets only load in if we are in lambda - not ci

--- tests/e2e/global.setup.ts
+++ tests/e2e/global.setup.ts
@@ -1,5 +1,3 @@
-import { execSync } from 'child_process';
-
 export default function globalSetup() {
-  execSync('npm run db:migrate && npm run db:seed', { stdio: 'inherit' });
+  return; // empty on purpose
 }
