35ad0ddad04cd14b7bb6d38cebad6fe762410e7c
KQASIM33 <mdqas191@gmail.com>
2026-03-24 13:35:34 +0000

    cv

---
+++ src/client/pages/customer/CreateTicketPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import NewTicketPage from './NewTicketPage';
+
+const CreateTicketPage: React.FC = () => {
+  return <NewTicketPage />;
+};
+
+export default CreateTicketPage;
