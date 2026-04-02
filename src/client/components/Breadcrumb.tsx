3b457330d4fee2b879afed9f9e7e007ca87ea62f
Zayeer-S <zayeersultan@gmail.com>
2026-04-02 15:19:42 +0100

    refactor(client): collocate breadcrumb file

--- src/client/components/Breadcrumb.tsx
+++ src/client/components/Breadcrumb.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { ClientRouteTypes } from '../constants/client.routes';
+import './Breadcrumb.css';
 
 export interface BreadcrumbProps {
   route: ClientRouteTypes;
