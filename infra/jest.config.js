3be2c8edd21aa56ac3e389dbc6b57848d881b200
Zayeer-S <zayeersultan@gmail.com>
2026-04-09 14:38:58 +0100

    feat(infra): make infra tests

--- infra/jest.config.js
+++ infra/jest.config.js
@@ -1,9 +1,11 @@
+process.env.CDK_BUNDLING_SKIP_ALL = 'true';
+
 module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': 'ts-jest',
   },
   setupFilesAfterEnv: ['aws-cdk-lib/testhelpers/jest-autoclean'],
 };
